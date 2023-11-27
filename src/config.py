ebfadad5789b5c77be784f1cd502c60890973fe1
DhruvKudale <dkudale07@gmail.com>
2023-11-27 23:39:46 +0530

    Adding sample results

--- src/config.py
+++ src/config.py
@@ -1,30 +1,30 @@
 # Set your input parameters
 
 # Dataset folder path. All jpg images under it recursively are included in the dataset
-dataset_path = "../dataset/training_set/"
+dataset_path = "../dataset/test_set/"
 
 # Query Image Folder path. All jpg images in this folder will be queried
-query_image_path = "../dataset/test_set_small/mountains_and_snow/809.jpg"
+query_image_path = "../dataset/just_query/"
 
 # To select which frequency distribution is calculated it can be 'basic-histogram', 'split-histogram' or 'ccv'
 # 'basic-histogram' is default value
-distribution = 'basic-histogram'
+distribution = 'basic-histo'
 
 # To select which distance or similarity will be used to measure proximity of query image and canddiate image from dataset
 # It can be 'l1', 'l2' or 'corr'. 'l2' is default
-proximity = 'l2'
+proximity = 'corr'
 
 # This is to finalize how many bins are needed for histograms. If there are c bins per channel, the histogram will have c ** 3 bins
 # Recommended to unchange this parameter. Default is 8
 channel_bins = 16
 
 # For indicating top 'k' images will be retrieved
-k = 25
+k = 4
 
 # If true will calculate P, R and F from the Korel dataset.
 # For inference, it can be kept false (false by default)
-experimentation = True
+experimentation = False
 
 # To display results on matplotlib, this is to be kept True.
 # This is True by default.
-display_results = False
+display_results = True
--- src/main.py
+++ src/main.py
@@ -18,6 +18,27 @@ def display_images(query_image, matched_image_path, proximity_value):
     plt.title(f"Matched with value: {proximity_value:.3f}")
     plt.show()
 
+def diplay_results_on_grid(results, query_image_path):
+    n = len(results)
+    print(f"Query image: {query_image_path}")
+    print("Top Matched Images from the dataset:")
+    query_image = cv2.imread(query_image_path)
+    plt.subplot(1, n + 1, 1)
+    plt.imshow(cv2.cvtColor(query_image, cv2.COLOR_BGR2RGB))
+    plt.title("Query Image")
+    for i in range(len(results)):
+        entry = results[i]
+        print(f"{entry['image-name']}")
+        print(f"{distribution} using {proximity} : {entry[proximity]:.3f}\n")
+        # Display matched image
+        matched_image_path = entry['image-name']
+        proximity_value = entry[proximity]
+        plt.subplot(1, n + 1, i + 2)
+        matched_image = cv2.imread(matched_image_path)
+        plt.imshow(cv2.cvtColor(matched_image, cv2.COLOR_BGR2RGB))
+        plt.title(f"Matched with value: {proximity_value:.3f}")
+    plt.show()
+
 def perfrom_cbir_experiment(dataset_path, query_images_path, distribution,
                  proximity, channel_bins, k, experimentation, display_results):
     query_images = glob.glob(query_images_path + '/*.jpg')
@@ -37,14 +58,7 @@ def perfrom_cbir_experiment(dataset_path, query_images_path, distribution,
         total_f = total_f + f
         # Display the results
         if display_results:
-            print(f"Query image: {query_image_path}")
-            print("Top Matched Images from the dataset:")
-            for i in range(len(results)):
-                entry = results[i]
-                print(f"{entry['image-name']}")
-                print(f"{distribution} using {proximity} : {entry[proximity]:.4f}\n")
-                # Display images
-                display_images(cv2.imread(query_image_path), entry['image-name'], entry[proximity])
+            diplay_results_on_grid(results, query_image_path)
     if experimentation:
         print(f'Precision : {total_p/total_q:.3f}')
         print(f'Recall    : {total_r/total_q:.3f}')
@@ -54,6 +68,7 @@ def perfrom_cbir_experiment(dataset_path, query_images_path, distribution,
 def perfrom_cbir(dataset_path, query_image_path, distribution,
                             proximity, channel_bins, k, experimentation, display_results):
     # Run the query and get results
+
     images, labels = run_query_inference_only(dataset_path, query_image_path, distribution, proximity,
                                  channel_bins, k, display_results=display_results, experimentation = False)
     return images, labels
@@ -61,9 +76,21 @@ def perfrom_cbir(dataset_path, query_image_path, distribution,
 if __name__ == "__main__":
     # Calling the core function. Arguments are picked from config file.
     # See the config file for more details on the arguments
-    images, labels = perfrom_cbir(dataset_path = dataset_path, query_image_path = query_image_path,
-                 distribution = distribution, proximity = proximity, channel_bins = channel_bins,
-                 k = k, experimentation = experimentation, display_results = display_results)
+    # images, labels = perfrom_cbir(dataset_path = dataset_path, query_image_path = query_image_path,
+    #              distribution = distribution, proximity = proximity, channel_bins = channel_bins,
+    #              k = k, experimentation = experimentation, display_results = display_results)
+    #
+    # print(images)
+    # print(labels)
 
-    print(images)
-    print(labels)
+    import time
+
+    t0 = time.time()
+    perfrom_cbir_experiment(dataset_path=dataset_path, query_images_path=query_image_path,
+                            distribution=distribution, proximity=proximity, channel_bins=channel_bins,
+                            k=k, experimentation=experimentation, display_results=False)
+    t1 = time.time()
+
+    total = t1 - t0
+    print(total)
+
